f8fd6bb5aab4527d828871f396f2e8cb98c92fd0
jvsobraz <jvsobraz@hotmail.com>
2023-08-23 11:36:38 -0300

    Update

---
+++ TodoApi/Models/TodoItem.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace TodoApi.Models;
+
+public class TodoItem
+{
+    public long Id { get; set; }
+    public string? Name { get; set; }
+    public bool IsComplete { get; set; }
+}
