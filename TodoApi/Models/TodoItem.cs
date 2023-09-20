0a77218094a3f78a197388ac606a61061a934a9c
jvsobraz <jvsobraz@hotmail.com>
2023-09-20 12:34:05 -0300

    Finished

--- TodoApi/Models/TodoItem.cs
+++ TodoApi/Models/TodoItem.cs
@@ -1,9 +1,18 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
-namespace TodoApi.Models;
+namespace TodoApi.Models
+{
+    public class TodoItem
+    {
+        public long Id { get; set; }
+        public string? Name { get; set; }
+        public bool IsComplete { get; set; }
+        public string? Secret { get; set; }
+    }
+}
 
-public class TodoItem
+public class TodoItemDTO
 {
     public long Id { get; set; }
     public string? Name { get; set; }
